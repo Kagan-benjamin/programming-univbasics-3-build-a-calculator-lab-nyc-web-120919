20d5c4a615da95ff2730efc4cd791cc474f0ada5
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-15 22:25:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,13 @@ return answer
 end
 
 def division(num1, num2)
-
+answer = (num1 / num2)
+return answer
 end
 
 def multiplication(num1, num2)
-
+answer = (num1 * num2)
+return answer
 end
 
 def modulo(num1, num2)
